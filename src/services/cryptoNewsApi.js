8dd840aa5ce63393adc52a4afc19ef655613a63f
nahee101 <bagsj26@gmail.com>
2022-08-15 19:05:45 +0900

    news 추가 중

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoNewsHeaders = {
+    'X-BingApis-SDK': 'true',
+    'X-RapidAPI-Key': '88f1220ff0msh6e232f8410ef6b7p1f285ejsnd314b020d248',
+    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+};
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptosNews: builder.query({
+            query: (newsCategory, count) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
+        })
+    })
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
