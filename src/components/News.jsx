b82e43ee3ae98413af74800249fc8335dd477431
nahee101 <bagsj26@gmail.com>
2022-08-16 12:20:13 +0900

    news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,6 +3,7 @@ import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
 import moment from "moment";
 
 import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const { Text, Title } = Typography;
 const { Option } = Select;
@@ -12,10 +13,10 @@ const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=Ne
 const News = ({ simplified }) => {
     const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
     const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 });
+    const { data } = useGetCryptosQuery(100);
 
     if (!cryptoNews?.value) return "Loading...";
 
-    console.log(cryptoNews)
     return (
         <>
             <Row gutter={[24, 24]}>
@@ -26,16 +27,17 @@ const News = ({ simplified }) => {
                             className="select-news"
                             placeholder="Select a Crypto"
                             optionFilterProp="children"
-                            onChange={(value) => console.log(value)}
+                            onChange={(value) => setNewsCategory(value)}
                             filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}>
-                            <Option vlaue="Cryptocurrency">Cryptocurrency</Option>
+                            <Option value="Cryptocurrency">Cryptocurrency</Option>
+                            {data?.data?.coins.map((coin, i) => <Option value={coin.name} key={i}>{coin.name}</Option>)}
                         </Select>
                     </Col>
                 )}
                 {cryptoNews.value.map((news, i) => (
                     <Col xs={24} sm={12} lg={8} key={i}>
                         <Card hoverable className="news-card">
-                            <a href={news.url} target="_blank" ref="noreferrer">
+                            <a href={news.url} target="_blank" rel="noreferrer">
                                 <div className="news-image-container">
                                     <Title className="news-title" level={4}>{news.name}</Title>
                                     <img style={{ maxWidth: '200px', maxHeight: '1000px' }} src={news?.image?.thumbnail?.contentUrl || demoImage} alt="news" />
@@ -47,10 +49,10 @@ const News = ({ simplified }) => {
                                 </p>
                                 <div className="provider-container">
                                     <div>
-                                        <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="news" />
+                                        <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="news writer" />
                                         <Text className="provider-name">{news.provider[0]?.name}</Text>
                                     </div>
-                                    <Text>{moment(news.dataPublished).startOf('ss').fromNow()}</Text>
+                                    <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
                                 </div>
                             </a>
                         </Card>
--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -14,8 +14,8 @@ export const cryptoNewsApi = createApi({
     reducerPath: 'cryptoNewsApi',
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
-        getCryptosNews: builder.query({
-            query: (newsCategory, count) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
+        getCryptoNews: builder.query({
+            query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
         })
     })
 });
